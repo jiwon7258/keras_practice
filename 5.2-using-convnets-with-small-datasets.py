0467e693fb0ade1abda5cbbd2a0e87d66bc03c8f
jiwon7258 <jiwon7258@gmail.com>
2020-10-25 17:41:24 +0900

    .

--- 5.2-using-convnets-with-small-datasets.py
+++ 5.2-using-convnets-with-small-datasets.py
@@ -101,10 +101,9 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
-
-# 모델 컴파일
 
 model.compile(loss='binary_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4),
@@ -113,23 +112,36 @@ model.compile(loss='binary_crossentropy',
 # 디렉터리에서 이미지를 읽어온다
 # batch size = 20 만큼 샘플을 계속해서 반환한다
 
-train_datagen = ImageDataGenerator(rescale=1. / 255)
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+)
+
+# 검증 데이터는 증식되어서는 안 됩니다!
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-train_generator = train_datagen.flow_from_directory(train_dir,
-                                                    target_size=(150, 150),
-                                                    batch_size=20,
-                                                    class_mode='binary')
+train_generator = train_datagen.flow_from_directory(
+    # 타깃 디렉터리
+    train_dir,
+    # 모든 이미지를 150 × 150 크기로 바꿉니다
+    target_size=(150, 150),
+    batch_size=32,
+    # binary_crossentropy 손실을 사용하기 때문에 이진 레이블을 만들어야 합니다
+    class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(validation_dir,
                                                         target_size=(150, 150),
-                                                        batch_size=20,
+                                                        batch_size=32,
                                                         class_mode='binary')
 
-# 배치 제너레이터를 사용하여 모델 훈련하기
 history = model.fit_generator(train_generator,
                               steps_per_epoch=100,
-                              epochs=30,
+                              epochs=100,
                               validation_data=validation_generator,
                               validation_steps=50)
 
